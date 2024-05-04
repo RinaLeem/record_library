af75366843ffc6d825fec122af6a0d2c3b969aff
Екатерина Балашова <rinaleem@mail.ru>
2024-05-04 18:16:47 +0400

    Filled TheSmiths vie

--- src/views/TheSmiths.vue
+++ src/views/TheSmiths.vue
@@ -24,11 +24,71 @@ function addToList() {
     <div class="album">
       <img
         class="album_img"
+        src="https://avatars.yandex.net/get-music-content/28589/f28a81f5.a.296972-1/200x200"
+      />
+      <div class="album_info">
+        <p class="album_name">Complete</p>
+        <p class="album_year">2011</p>
+      </div>
+    </div>
+    <div class="album">
+      <img
+        class="album_img"
+        src="https://avatars.yandex.net/get-music-content/28589/4108299a.a.58466-6/200x200"
+      />
+      <div class="album_info">
+        <p class="album_name">The Sound Of The Smiths</p>
+        <p class="album_year">2008</p>
+      </div>
+    </div>
+    <div class="album">
+      <img
+        class="album_img"
+        src="https://avatars.yandex.net/get-music-content/38044/5b813fba.a.298107-3/200x200"
+      />
+      <div class="album_info">
+        <p class="album_name">Strangeways, Here We Come</p>
+        <p class="album_year">1987</p>
+      </div>
+    </div>
+    <div class="album">
+      <img
+        class="album_img"
+        src="https://avatars.yandex.net/get-music-content/175191/a6e1f13b.a.4742548-1/200x200"
+      />
+      <div class="album_info">
+        <p class="album_name">The Queen Is Dead</p>
+        <p class="album_year">1986</p>
+      </div>
+    </div>
+    <div class="album">
+      <img
+        class="album_img"
+        src="https://avatars.yandex.net/get-music-content/28589/2c2d8cf3.a.453365-1/200x200"
+      />
+      <div class="album_info">
+        <p class="album_name">Meat Is Murder</p>
+        <p class="album_year">1985</p>
+      </div>
+    </div>
+    <div class="album">
+      <img
+        class="album_img"
+        src="https://avatars.yandex.net/get-music-content/28589/4e4aa898.a.299260-1/200x200"
+      />
+      <div class="album_info">
+        <p class="album_name">Hatful of Hollow</p>
+        <p class="album_year">1984</p>
+      </div>
+    </div>
+    <div class="album">
+      <img
+        class="album_img"
         src="https://avatars.yandex.net/get-music-content/38044/5518394f.a.298106-1/200x200"
       />
       <div class="album_info">
-        <p class="album_name">Name</p>
-        <p class="album_year">1999</p>
+        <p class="album_name">The Smiths</p>
+        <p class="album_year">1984</p>
       </div>
     </div>
     <!--  
@@ -47,9 +107,7 @@ function addToList() {
       <input type="button" @click="addToList" value="Добавить" /><br /><br />
     </form>
     <div v-for="item of comments" :key="item.id" class="list">
-      <label :for="item.id">
-        {{ item.text }}
-      </label>
+      <label :for="item.id"> {{ item.text }}<br /> </label>
     </div>
   </div>
 </template>
@@ -68,6 +126,7 @@ p {
   flex-wrap: wrap;
   width: 100%;
   min-width: 600px;
+  margin-bottom: 10px;
 }
 .album_img {
   width: 200px;
@@ -81,6 +140,6 @@ p {
 }
 .album_year {
   color: gray;
-  font-size: 12px;
+  font-size: 16px;
 }
 </style>
