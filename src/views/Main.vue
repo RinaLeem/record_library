fb5353098984a7a93f191fa90e1997aaaf571155
Екатерина Балашова <rinaleem@mail.ru>
2024-05-04 20:05:25 +0400

    Added zoom

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -7,29 +7,43 @@
         <router-link to="/TheCure" class="links">The Cure</router-link
         ><br /><br />
         <img
+          class="zoom"
           src="https://m.media-amazon.com/images/I/81PoA51cw3L._AC_UF1000,1000_QL80_.jpg"
           alt="The Cure poster"
+          @mouseover="zoomIn"
+          @mouseout="zoomOut"
         /><br />
       </div>
       <div id="element2">
         <router-link to="/TheSmiths" class="links">The Smiths</router-link
         ><br /><br />
         <img
+          class="zoom"
           src="https://i.etsystatic.com/36642319/r/il/766e28/4490479645/il_fullxfull.4490479645_6on9.jpg"
           alt="The Smiths poster"
+          @mouseover="zoomIn"
+          @mouseout="zoomOut"
         /><br />
       </div>
     </div>
   </div>
 </template>
 
-<script>
-export default {
-  name: "Main",
-};
+<script setup>
+function zoomIn(event) {
+  event.target.style.transform = "scale(1.5)";
+}
+
+function zoomOut(event) {
+  event.target.style.transform = "scale(1)";
+}
 </script>
 
 <style scoped>
+.zoom {
+  transition: transform 0.5s ease-in-out;
+}
+
 .colums {
   padding: 10px;
   display: flex;
